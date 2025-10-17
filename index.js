1570273c756b00e7968e618eea1111963931beab
Md Emdadullah <86929969+developeremdad@users.noreply.github.com>
2025-10-17 17:01:47 +0600

    update code

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Welcome to docker initial project on port ${port}`);
 });
-// docker run -p 5000:5000 --name initialize-container -w //app -v initialize-docker-logs://app/logs -v "${PWD}://app" -v //app/node_modules --rminitialize-docker
+// docker run -p 5000:5000 --name initialize-container -w //app -v initialize-docker-logs://app/logs -v "${PWD}://app" -v //app/node_modules --rm initialize-docker
+
+// docker run -p 5000:5000 --name ts-container -v ts-docker-logs://app/logs -w //app -v "//D:\Docker\docker-initialize"://app -v //app/node_modules --rm ts-docker-image
+
+// docker run -p 5000:5000 --name initialize-container -w /app -v "/d/Docker/docker-initialize:/app" -v "/d/Docker/docker-initialize/node_modules:/app/node_modules" --rm initialize-image
